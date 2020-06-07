26636e72378542adb9879c555ba9d129159f23d9
Beth <bethhnielsen56@gmail.com>
2020-06-07 12:43:39 -0500

    user is able to authenticate

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import './App.scss';
 
+import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
+import Auth from '../components/pages/Auth/Auth';
+
+import fbConnection from '../helpers/connection';
+
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     return (
       <div className="App">
+        <MyNavbar />
         <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info"><i className="fas fa-rocket"></i>TESTING REACT HOARDER BUTTON</button>
+        <Auth />
       </div>
     );
   }
