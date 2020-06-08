2b8a51797e08b1ada1bc69e9e1a7ad388af33da7
Beth <bethhnielsen56@gmail.com>
2020-06-08 16:50:43 -0500

    navbar is working

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -1,21 +1,69 @@
 import React from 'react';
-
+import { NavLink as RRNavLink } from 'react-router-dom';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import PropTypes from 'prop-types';
 import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool.isRequired,
+  }
+
+  state = {
+    isOpen: false,
+  }
+
   logOut = (e) => {
     e.preventDefault();
     firebase.auth().signOut();
   }
 
+  toggle = (e) => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
   render() {
+    const { isOpen } = this.state;
+    const buildNavbar = () => {
+      const { authed } = this.props;
+      if (authed) {
+        return (
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink tag={RRNavLink} to='/home'>Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RRNavLink} to='/new'>New Stuff</NavLink>
+          </NavItem>
+         <NavItem>
+            <NavLink className="btn btn-warning" onClick={this.logOut}>Logout</NavLink>
+          </NavItem>
+        </Nav>
+        );
+      }
+      return <Nav className="ml-auto" navbar></Nav>;
+    };
+
     return (
-      <div className="MyNavbar">
-        <h1>MyNavbar</h1>
-        <button className="btn btn-primary" onClick={this.logOut}>Logout</button>
+      <div className="myNavbar">
+        <Navbar color="info" light expand="md">
+        <NavbarBrand href="/">React Hoarder</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={isOpen} navbar>
+         {buildNavbar()}
+        </Collapse>
+      </Navbar>
       </div>
     );
   }
